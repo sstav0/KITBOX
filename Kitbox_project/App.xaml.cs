1815399bebedb6a83de6041f3d397a2a85c15aea
Dori-Tos <dorian.smaelen@hotmail.com>
2024-02-09 13:24:20 +0100

    added the base

---
+++ Kitbox_project/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Kitbox_project
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
