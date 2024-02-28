0ff445d80f8bddb86b8ac55e28a732f7dc1416e2
Yessboom <Tanguy.guissard@hotmail.be>
2024-02-29 00:29:51 +0100

    Binded Lockers to CabinetCreatorPage

--- Kitbox_project/Views/CabinetCreatorPage.xaml.cs
+++ Kitbox_project/Views/CabinetCreatorPage.xaml.cs
@@ -1,6 +1,8 @@
 using Kitbox_project.ViewModels;
 using System.Collections.ObjectModel;
 using Kitbox_project.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Kitbox_project.Utilities;
 
 namespace Kitbox_project.Views
 {
@@ -16,6 +18,9 @@ namespace Kitbox_project.Views
 
             // Load available lockers into the view model
             LoadAvailableLockers();
+            ShowExpansionPanels();
+
+
         }
 
         private void LoadAvailableLockers()
@@ -27,9 +32,13 @@ namespace Kitbox_project.Views
             {
                 new LockerViewModel { Height = 50, Color = "Red", Door = true, Price = 500 },
                 new LockerViewModel { Height = 60, Color = "Blue", Door = false, Price = 1000 },
+                new LockerViewModel { Height = 70, Color = "Green", Door = true, Price = 250 },
                 new LockerViewModel { Height = 70, Color = "Green", Door = true, Price = 250 }
+
             };
         }
+
+
 
         private void AddSelectedLocker_Clicked(object sender, EventArgs e)
         {
@@ -46,6 +55,25 @@ namespace Kitbox_project.Views
                 System.Diagnostics.Debug.WriteLine(_viewModel.Lockers.Count);
                 // On remets le picker à 0 parce que c'est plus cool
                 //lockerPicker.SelectedItem = null;
+            }
+        }
+        private void ShowExpansionPanels()
+        {
+            // Determine the number of available lockers
+            int numberOfLockers = _viewModel.AvailableLockers.Count;
+
+            // Loop through each ExpansionPanel and set its visibility
+            for (int i = 0; i < numberOfLockers; i++)
+            {
+                // Find the ExpansionPanel control by its name
+                var expansionPanel = FindByName("ExpansionPanel" + (i + 1));
+
+                // Cast the result to ExpansionPanel
+                if (expansionPanel is ExpansionPanel panel)
+                {
+                    // Toggle visibility based on whether the item exists in the list
+                    panel.IsVisible = true; // or false depending on your condition
+                }
             }
         }
 
