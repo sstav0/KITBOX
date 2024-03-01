04bd78da781df2c9fa1527640cef62490fe59b71
BNoteris <23283@ecam.be>
2024-03-01 09:24:00 +0100

    s

---
+++ Kitbox_project/DataBase/DatabaseStock.cs
@@ -0,0 +1,12 @@
+﻿namespace Kitbox_project;
+
+using System;
+using Kitbox_project.DataBase;
+using Kitbox_project.Models;
+using MySql.Data.MySqlClient;
+public class DatabaseStock : Database
+{
+    public DatabaseStock(){
+        tablename = "Stock";
+    }
+}
