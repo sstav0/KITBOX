a7022e6595053565c438e3075d24ecfdbdb970af
stav0 <xavierallaud@gmail.com>
2024-03-01 10:05:40 +0100

    Correct example

--- Kitbox_project/Views/CartPage.xaml.cs
+++ Kitbox_project/Views/CartPage.xaml.cs
@@ -26,7 +26,7 @@ public partial class BasketPage : ContentPage
 		List<Locker> lockers1 = new List<Locker>();
 		lockers1.Add(locker1);
 		lockers1.Add(locker1bis);
-		Cabinet cabinet1 = new Cabinet(lockers1, 50, 75, 1);
+		Cabinet cabinet1 = new Cabinet(lockers1, 50, 75, 1, 1);
 		CabinetViewModelV2 cabinet1view = new CabinetViewModelV2(cabinet1);
 
 		Basket.Add(cabinet1view);
