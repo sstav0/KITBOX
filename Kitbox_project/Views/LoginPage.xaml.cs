98dd7abb8562ae745f2082e72284bacefeb20855
BNoteris <23283@ecam.be>
2024-03-11 09:50:53 +0100

    LoginPage Testing

--- Kitbox_project/Views/LoginPage.xaml.cs
+++ Kitbox_project/Views/LoginPage.xaml.cs
@@ -1,10 +1,16 @@
 namespace Kitbox_project.Views;
+using Kitbox_project.DataBase;
+using MySql.Data.MySqlClient;
+
 
 public partial class LoginPage : ContentPage
 {
+	private readonly DatabaseLogin _dbService;
 	public LoginPage()
 	{
 		InitializeComponent();
+		_dbService = new DatabaseLogin();
+		BindingContext = this;
 	}
 
 	private void TogglePasswordObscurationClicked(object sender, EventArgs e)
@@ -18,5 +24,25 @@ public partial class LoginPage : ContentPage
 		{
 			Password.IsPassword = true;
 		}
+	}
+	private void LoginTryClicked(object sender, EventArgs e){
+		string username = UserName.Text;
+        string password = Password.Text;
+		bool ValidLogin = _dbService.ValidateUser(username, password);
+		if(ValidLogin)
+		{
+			if (username=="storekeeper"){
+				Navigation.PushAsync(new StockPage());
+			}
+			else if(username=="customer"){
+				Navigation.PushAsync(new HomeClientPage());
+			}
+		}
+
+		else
+            {
+                // Display authentication failed message
+                DisplayAlert("Login Failed", "Invalid username or password", "OK");
+            }
 	}
 }
