dff227d6d781c4df5726f6b2b0ec3bf6cabe5b15
Nicolas Jurquet <nicolor01@hotmail.com>
2024-03-21 16:19:29 +0100

    Created SupplierOrder database

---
+++ Kitbox_project/DataBase/DatabaseSupplierOrders.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kitbox_project.Models;
+using Kitbox_project.DataBase;
+
+namespace Kitbox_project
+{
+    public class DatabaseSupplierOrders : Database
+    {
+        public DatabaseSupplierOrders(string id, string password) : base(id, password)
+        {
+            tablename = "SupplierOrders";
+        }
+
+        public static List<SupplierOrder> ConvertToSupplierOrder(List<Dictionary<string, string>> data)
+        {
+            List<SupplierOrder> supplierOrders = new List<SupplierOrder>();
+            foreach (var item in data)
+            {
+                //supplierOrders.Add(new SupplierOrder(
+                //    int.Parse(item["idStock"]),
+                //    item["Reference"],
+                //    item["Code"],
+                //    int.Parse(item["Quantity"])
+                //));
+            }
+            return supplierOrders;
+        }
+    }
+}
