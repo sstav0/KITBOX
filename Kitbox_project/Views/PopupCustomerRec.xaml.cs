1c32862989a9263bc56f0f01734494e0f6af8f86
ARTHURVANBELLE <23319@ecam.be>
2024-03-27 10:13:06 +0100

    .NET 8 !!  +  Nouvelle DA

---
+++ Kitbox_project/Views/PopupCustomerRec.xaml.cs
@@ -0,0 +1,20 @@
+//using CommunityToolkit.Maui.Views;
+using Kitbox_project.Models;
+using Kitbox_project.ViewModels;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+/*
+namespace Kitbox_project.Views
+{
+    public partial class PopupCustomerRec : Popup 
+    {
+        public PopupCustomerRec()
+        {
+            InitializeComponent();
+        }
+    }
+}
+*/
